bfd5e565234429e141803a988bbf3576f31f20cd
rrtyui <marcelo6549@gmail.com>
2022-06-06 06:40:26 -0700

    fix

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	printf("Size of a char %d bytes \n"sizeof(char));
-	printf("Size of a int %d bytes \n"sizeof(int));
-	printf("Size of a float %d bytes \n"sizeof(float));
-	printf("Size of a long %d bytes \n"sizeof(long int));
-	printf("Size of a long long  %d bytes \n"sizeof(long long int));
+	printf("Size of a char %d bytes \n",sizeof(char));
+	printf("Size of a int %d bytes \n",sizeof(int));
+	printf("Size of a float %d bytes \n",sizeof(float));
+	printf("Size of a long %d bytes \n",sizeof(long int));
+	printf("Size of a long long  %d bytes \n",sizeof(long long int));
 
 	return (0);
 }
