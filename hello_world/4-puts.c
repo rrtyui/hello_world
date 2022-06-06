2ead97dd7d0d3c034dc2fa393fb33e57c0f93415
rrtyui <marcelo6549@gmail.com>
2022-06-06 06:13:31 -0700

    fix

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+/**
+ * this program prints the followin text
+ * */
 int main(void){
 
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle \n");
 	
-	return 0;
+	return (0);
 }
