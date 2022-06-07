ba4f060ec6cf3416cc2dddd4a689e5d57acc93f5
rrtyui <marcelo6549@gmail.com>
2022-06-07 09:20:39 -0700

    fix

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - this program prints the followin text
+ * Return: zero
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
