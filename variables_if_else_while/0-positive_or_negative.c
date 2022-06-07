d426156b4286ba4a47f56933f6db4f55e6ad46e3
rrtyui <marcelo6549@gmail.com>
2022-06-07 05:34:49 -0700

    task 0

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - sort a random number and asign it to n
+ * Return: zero
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("The number is positive");
+	}
+	if (n < 0)
+	{
+		printf("The number is negative ");
+	}
+	if (n == 0)
+	{
+		printf("The number is zero");
+	}
+	return (0);
+
+}
