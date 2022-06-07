5634699dc713cc5b0efd70ce1e460710b48a4fea
rrtyui <marcelo6549@gmail.com>
2022-06-07 09:33:01 -0700

    fix

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - this program prints the followin text
+ * Return: zero
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		printf("\n");
+		i++;
+	}
+	return (0);
+}
