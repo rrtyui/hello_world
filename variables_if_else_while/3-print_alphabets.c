85ecdfd6ad2e361774ac38ea11e16e80c5d040a9
rrtyui <marcelo6549@gmail.com>
2022-06-07 08:49:36 -0700

    fix

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - this program prints the followin text
+ * Return: zero
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; che <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
