5cd3a4bdbe76d15670416de72d28f3a63f583693
rrtyui <marcelo6549@gmail.com>
2022-06-08 06:44:51 -0700

    fix

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,13 +1,19 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - this program prints the followin text
+ * Return: zero
  */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+        _putchar('-');
+        _putchar('p');
+        _putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
+        _putchar('\n');
+        return (0);
 }
