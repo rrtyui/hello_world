4e1e5d1a79b30335d10a6976a6f3e753a200676f
rrtyui <marcelo6549@gmail.com>
2022-06-09 09:00:37 -0700

    fix

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+*_isalpha - checks for a lower case character
+*Return: 1 if is lower or 0 otherwise
+*@c: is the parametrer given to the funct
+*/
+int _isalpha(int c)
+{
+	return (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z');
+}
