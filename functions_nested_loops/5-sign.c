15541d409271166ba248616f81274848cfa00b2f
rrtyui <marcelo6549@gmail.com>
2022-06-09 09:40:11 -0700

    fix

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+*print_sign - checks for a lower case character
+*Return: 1 if is lower or 0 otherwise
+*@n: is the parametrer given to the funct
+*/
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+
+		return(1);
+	}
+
+	else if (n = 0)
+	{
+		printf("0");
+
+		return(0);	
+	}
+
+	else if (n < 0)
+	{
+		printf("-");
+
+		return (-1);
+	}
+}
