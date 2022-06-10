82fe8c9a62cc923ccc9a6f4936b42fc70c6a7d5a
rrtyui <marcelo6549@gmail.com>
2022-06-10 09:14:34 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+*add - checks for a lower case character
+*@n: variable
+*@n1: second varible
+*Return: the sum of those two
+*/
+
+int add(int n, int n1)
+{
+	return (n + n1);
+}
