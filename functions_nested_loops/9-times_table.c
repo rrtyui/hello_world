ad87d35eb3ab876de4aeb2b4a6e841766d823ffe
rrtyui <marcelo6549@gmail.com>
2022-06-10 08:41:24 -0700

    9-times_table.c

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*times_table - print the table
+*/
+
+void times_table(void)
+{
+	int t, n;
+
+	t = 0;
+
+	while (t <= 9)
+	{
+		n = 1;
+
+		while (n <= 9)
+		{
+			_putchar((t * n) + '0');
+			_putchar(',');
+			_putchar(' ');
+
+			n++;
+
+		}
+		t++;
+	}
+}
