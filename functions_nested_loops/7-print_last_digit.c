896af245491dda92a219e980acfb635c804329e9
rrtyui <marcelo6549@gmail.com>
2022-06-10 05:56:23 -0700

    fix

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*print_last_digit - checks for a lower case character
+*Return: 1 if is lower or 0 otherwise
+*@n: is the parametrer given to the funct
+*/
+
+int print_last_digit(int n)
+{
+	printf("%d", n % 10)
+}
