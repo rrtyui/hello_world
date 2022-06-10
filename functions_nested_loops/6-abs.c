8d694fc2a0c390cd09e66e3324832f1c137b2f97
rrtyui <marcelo6549@gmail.com>
2022-06-10 05:50:51 -0700

    fix

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,14 +2,14 @@
 /**
 *_abs - do the abs of the enter value
 *Return: 1 if is lower or 0 otherwise
-*@c: is the parametrer given to the funct
+*@n: is the parametrer given to the funct
 */
 
 int _abs(int n)
 {
 	if (n >= 0)
 	{
-		return(n);
+		return (n);
 	}
 	return (-n);
 }
