1812d4d9cea79a95d73e412e32bfeb9fbb9921db
rrtyui <marcelo6549@gmail.com>
2022-06-10 09:24:58 -0700

    11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*add - checks for a lower case character
+*@n: variable
+*@n1: second varible
+*Return: the sum of those two
+*/
+
+void print_to_98(int i)
+{
+	int n, i;
+
+	for (n = i; n <= 98; n++)
+	{
+		printf("%d",n);
+		printf(",");
+		printf(" ");
+		printf("\n");
+
+	}
+}
