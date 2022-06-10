253d95e152759572241e7fe8ca7bd86bc1c0256e
rrtyui <marcelo6549@gmail.com>
2022-06-10 05:47:44 -0700

    fix

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*_abs - do the abs of the enter value
+*Return: 1 if is lower or 0 otherwise
+*@c: is the parametrer given to the funct
+*/
+
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return(n);
+	}
+	return (-n);
+}
