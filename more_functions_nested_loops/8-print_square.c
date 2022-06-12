a5133fd17449a68d2aa452e07d4712bd56fb851e
rrtyui <marcelo6549@gmail.com>
2022-06-12 15:39:33 -0700

    8-print_square.c

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_square - print lines in terminal
+* @size: size of the square
+*/
+
+void print_square(int size)
+{
+	int l, c;
+
+	if (size > 0)
+	{
+		for (c = 0; c < size; c++)
+		{
+			l = size;
+
+			while (l = size)
+			{
+				_putchar('#');
+
+				l++;
+			}
+
+			_putchar('#');
+
+
+		}
+	}
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
