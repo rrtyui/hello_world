68929bac041cf9f7a34c9e0a7d140e971de19f08
rrtyui <marcelo6549@gmail.com>
2022-06-12 09:02:56 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* mul - checks for upper case
+*@a: variable
+*@b: variable
+* Return: the multiplication
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
