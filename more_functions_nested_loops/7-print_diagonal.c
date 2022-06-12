eae4e5b1e3dde6165519320581eca9eac0d90438
rrtyui <marcelo6549@gmail.com>
2022-06-12 15:17:57 -0700

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_diagonal - print diagonal lines
+* @n: parameterer
+*/
+
+void print_diagonal(int n)
+{
+	int l, s;
+
+	if (n > 0)
+	{
+		for (l = 0; l < n; l++)
+		{
+			for (s = 0; s < l; s++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+
+			if (l == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
