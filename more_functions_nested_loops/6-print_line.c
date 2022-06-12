b4755e936d827096d12546db3e4d20ae02a1280a
rrtyui <marcelo6549@gmail.com>
2022-06-12 15:01:54 -0700

    6-print_line.c

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_line - print lines in terminal
+*/
+
+void print_line(int n)
+{
+	int c = 0;
+
+	while (c <= n)
+	{
+		_putchar('_');
+
+		if (n <= 0);
+		{
+			_putchar('\n');
+		}
+		c++
+	}
+	_putchar('\n');
+}
