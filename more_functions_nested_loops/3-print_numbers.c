c242cfd31deffd4a75e7e9984e52897a4a517586
rrtyui <marcelo6549@gmail.com>
2022-06-12 09:06:10 -0700

    3-print_numbers.c

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isupper - checks for upper case
+*@c: variable
+* Return: 1 if its upper and cero otherwise
+*/
+
+void print_numbers(void)
+{
+	char n;
+
+	for (n >= 48; n <= 58 ; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
