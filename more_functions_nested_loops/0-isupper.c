f6b9925659a2b7e8a15b6d31b575ad5372723873
rrtyui <marcelo6549@gmail.com>
2022-06-12 08:45:09 -0700

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _isupper - checks for upper case
+*@c: variable
+* Return: 1 if its upper and cero otherwise
+*/
+
+int _isupper(int c)
+{
+	return (c => 'A' && c <= 'Z');
+}
---
+++ more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ This is more function and nested loops practice
