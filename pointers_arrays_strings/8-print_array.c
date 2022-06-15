38476378f3cf6070471b7c57b6348fd9a8f0ea8b
rrtyui <marcelo6549@gmail.com>
2022-06-15 11:10:43 -0700

    8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_rev - return the length of a string
+* @str: parameterer
+*/
+
+void print_array(int *a, int n)
+{
+	int l;
+
+	for (l = 0; l < n; l++)
+	{
+		printf("%d", a[l]);
+	}
+	printf("\n");
+}
