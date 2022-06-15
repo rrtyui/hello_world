1abde1cdc0375e4a672eb04e6f142799b792c6ba
rrtyui <marcelo6549@gmail.com>
2022-06-15 11:33:40 -0700

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* *_strcpy - return the length of a string
+* @dest: parameterer
+* @src: parametrer
+*/
+
+char *_strcpy(char *dest,char *src)
+{
+	int index = 0;
+
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	return (dest);
+}
