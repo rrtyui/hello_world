46c912036cbbb8cb50da95004d5f40d88ecc9df7
rrtyui <marcelo6549@gmail.com>
2022-06-15 09:05:02 -0700

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* puts2 - return the length of a string
+* @str: parameterer
+*/
+
+void puts2(char *str)
+{
+
+	int l = 0;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+
+	int i = 0;
+
+	for (i = 0; i < l; i+=2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n')
+}
