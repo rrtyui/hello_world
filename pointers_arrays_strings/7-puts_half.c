8354f6454f7768c92873cd5a66f902505492a043
rrtyui <marcelo6549@gmail.com>
2022-06-15 09:44:16 -0700

    7-puts_half.c

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+* print_rev - return the length of a string
+* @str: parameterer
+*/
+
+void puts_half(char *str)
+{
+	int l = 0;
+
+	int h = l /2;
+
+	int n = (l - 1) / 2;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+
+
+
+	if (l % 2 == 0)
+	{
+		while (n < l)
+		{
+			_putchar(n);
+			n++;
+		}
+	}
+
+	for (h <= l; h++)
+	{
+		_putchar(str[h]);
+	}
+
+
+}
