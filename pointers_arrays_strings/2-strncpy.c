4b341a617f19a0eb1fc3c56e3ccaea49583a8239
rrtyui <marcelo6549@gmail.com>
2022-06-16 08:37:23 -0700

    2-strncpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* _strncat - return the length of a string
+* @src: parameterer
+* @dest: parametrer
+* @n: paremetrer
+* Return: dest
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < src[n]; i++)
+	{
+		src[i] = dest[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+}
