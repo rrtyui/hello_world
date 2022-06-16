58ed9ad6b837a83a0e3fc40f0d06f37f1968aaba
rrtyui <marcelo6549@gmail.com>
2022-06-16 06:08:13 -0700

    0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* puts_half - return the length of a string
+* @str: parameterer
+*/
+
+char *_strcat(char *dest, char *src)
+{
+	int l;
+
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+
+	char *pt = dest + l;
+
+	while (src != '\0')
+	{
+		ptr = '\0';
+	}
+
+	return (dest);
+}
