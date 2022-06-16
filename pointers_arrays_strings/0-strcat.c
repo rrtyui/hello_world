0bc68fab1e4360af78166f38a3e58cc1758aacec
rrtyui <marcelo6549@gmail.com>
2022-06-16 06:16:47 -0700

    0-strcat.c

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 
 	j = 0;
 
-	while(dest[i] != '\0')
+	while (dest[i] != '\0')
 	{
 		i++;
 	}
@@ -28,7 +28,7 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 
-	dest [i] = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
