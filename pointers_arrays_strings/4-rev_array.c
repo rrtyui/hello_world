5e1ebb83f74e0c73828d9b5ef835ca5ea313946c
rrtyui <marcelo6549@gmail.com>
2022-06-18 09:18:35 -0700

    4-rev_array.c

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* reverse_array - return the length of a string
+* @a: parameterer
+* @n: paremetrer
+*/
+
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
