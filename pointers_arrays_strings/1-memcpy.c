141f596563f876f0986aaa7bc6748957004bf15b
rrtyui <marcelo6549@gmail.com>
2022-06-19 15:05:57 -0700

    1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_memcpy - Copy and change value
+ *@dest: value
+ *@src: value
+ *@n: value
+ *Return: dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
