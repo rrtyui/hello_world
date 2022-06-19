ecda795849cf4930df902c80e7f5f0df157ab93d
rrtyui <marcelo6549@gmail.com>
2022-06-19 15:11:22 -0700

    2-strchr.c

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,15 @@
  *@n: paremetrer
  *Return: dest
  */
-char *_strncpy(char *dest, char *src, int n)
+char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+	}
 
-	for ( ; i < n; i++)
-		dest[i] = '\0';
-
-	return (dest);
+	return ('\0');
 }
