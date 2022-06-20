ec281b6a1a9ced2362b1c3e85d97b189b2b3ff46
rrtyui <marcelo6549@gmail.com>
2022-06-20 15:59:23 -0700

    6-is_prime_number.c

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ *check - primer number for
+ *@n: par
+ *@i: par
+ *Return: function
+ */
+
+int check(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (check(n, i - 1));
+		}
+	}
+}
+
+/**
+ *is_prime_number - prime 
+ *@n: par
+ *Return: function
+ */
+
+int is_prime_number(int n)
+{
+	if (n == 1)
+		return (0);
+	else
+		return (check(n, n / 2));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -53,3 +53,5 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int isqrt_rec(int i, int n);
+int is_prime_number(int n);
+int check(int n, int i);
