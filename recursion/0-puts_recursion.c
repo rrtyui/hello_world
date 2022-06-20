c2d470e4f3d95571ecc8195cf1de5bcc1c174a28
rrtyui <marcelo6549@gmail.com>
2022-06-20 06:31:36 -0700

    0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -4,11 +4,11 @@
 
 void _puts_recursion(char *s)
 {
-	if (s == '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 		return;
 	}
-	_putchar(s);
-	_puts_recursion(s + 1);
+	_putchar(*s);
+	_puts_recursion(*s + 1);
 }
