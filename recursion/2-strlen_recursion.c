6e8e3d8a62bf803d6cc405602ccecd32d8fd30ae
rrtyui <marcelo6549@gmail.com>
2022-06-20 07:11:26 -0700

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -3,16 +3,10 @@
 
 int _strlen_recursion(char *s)
 {
-	int i = 0;
 
-	if (s[i] == '\0')
+	if (*s == '\0')
 	{
-		return (1);
+		return (0);
 	}
-
-	else
-	{
-		i++;
-	}
-	_srtlen_recursion(s) + 1;
+	return (1 + _strlen_recursion(s + 1));
 }
