329c9244d1db8e3e622851e175f07cbe080258cb
rrtyui <marcelo6549@gmail.com>
2022-06-21 07:24:43 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+
+int main(int argc, char *argv[])
+{
+    int i;
+    printf("%d\n",argc);
+    for(i=0;i<argc-1;i++)
+    {
+        printf("%s",argv[i]);
+    }
+    return 0;
+}
