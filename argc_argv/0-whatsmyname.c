85f59ec2ca084c7a31e22870d1620c56351738c5
rrtyui <marcelo6549@gmail.com>
2022-06-21 07:18:35 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <stdio.h>
-int main (int __attribute__ ((__unused__)) argc,char *argv[])
+/**
+ *main  - main function
+ *@argc: counter of args
+ *@argv: array of pointers
+ *Return: 0
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 
 	return (0);
 }
