f26ba61f78664881c91526366e65a8bfa9108a7a
rrtyui <marcelo6549@gmail.com>
2022-06-23 09:41:31 -0700

    task

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+* _strdup - duplicate a string 
+* @str: parameterer
+* Return: null if its 0 and pointer if its succes
+*/
+
+char *_strdup(char *str)
+{
+	char *strdup;
+
+	int i, l;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i]; i++)
+	{
+		l++
+	}
+
+	strdup = malloc(sizeof(char) * (l - 1));
+
+	if(strdup == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i]; i++)
+	{
+		strdup[i] = str[i];
+	}
+
+	strdup[l] = '\0';
+
+	return (strdup);
+}
