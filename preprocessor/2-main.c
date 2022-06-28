771bf5a99ea41269d81528bcd87871f4b10752b1
rrtyui <marcelo6549@gmail.com>
2022-06-28 11:28:31 -0700

    task

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - function that print the file name
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	printf(__FILE__);
+	printf("\n");
+	return (0);
+}i
