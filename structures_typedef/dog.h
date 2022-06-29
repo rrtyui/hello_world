883c3be35610b225a592ca66cf653c5704505710
rrtyui <marcelo6549@gmail.com>
2022-06-29 08:18:20 -0700

    task

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,37 @@
-struct dog{
+#ifndef dog_h
+#define dog_h
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 
+/**
+ *struct dog - type of the dog
+ *@name: name
+ *@age: age
+ *@owner: owner
+ *Return: struct
+ */
+
+struct dog
+{
 	char *name;
-
-	float *age;
-
+	float age;
 	char *owner;
 };
+/**
+ *struct dog_t - dog
+ *@name: par
+ *@age: par
+ *@owner: par
+ */
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+int main(void);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif
