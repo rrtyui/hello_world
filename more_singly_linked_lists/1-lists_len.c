bff2558aeb6675df24088f6538cda3c2476a6cdd
rrtyui <marcelo6549@gmail.com>
2022-07-15 15:06:53 -0700

    task

---
+++ more_singly_linked_lists/1-lists_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *listint_len - returns the number of elements listed 
+ *@h: pointer
+ *Return: index
+ */
+size_t listint_len(const listint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
