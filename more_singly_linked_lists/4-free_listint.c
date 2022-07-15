86852557585528a8be9fe805b04d41b1d2bd9bb3
rrtyui <marcelo6549@gmail.com>
2022-07-15 15:16:34 -0700

    task

---
+++ more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ *free_listint - free memory list
+ *@head: pointer
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *n;
+
+	while (head != NULL)
+	{
+		n = head;
+		head = head->next;
+		free(n);
+	}
+}i
